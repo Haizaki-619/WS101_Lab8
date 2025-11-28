2f1f5336d16addaed8bccb0d3aa31a2971937741
Mabini_Ralph <158345059+Haizaki-619@users.noreply.github.com>
2025-11-28 12:57:28 +0800

    Add files via upload

---
+++ Laboratory7Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratory7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratory7Application.class, args);
+	}
+
+}
